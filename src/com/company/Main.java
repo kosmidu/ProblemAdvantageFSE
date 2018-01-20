945ed305028462534c22b4959d24b7841f7d4317
kosmidu <kosmidu.maria@gmail.com>
2018-01-21 01:08:50 +0200

    Handle NumberFormatException. Add better messages.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,11 @@ package com.company;
 
 import java.util.*;
 
-public class Main {
-
+public class Main
+{
     public static void main(String[] args)
     {
-        System.out.println("Please, enter only non negative integers, separated by spaces.");
+        System.out.println("Please, insert only non negative integers, separated by spaces.\nThen press enter.");
 
         ArrayList<Integer> inputList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
@@ -14,7 +14,14 @@ public class Main {
 
         // Insert the integers to inputList.
         for (String token : tokens)
-            inputList.add(Integer.parseInt(token));
+        {
+            try {
+                inputList.add(Integer.parseInt(token));
+            }catch(NumberFormatException ex) {
+                System.err.println("Please, insert only integers.");
+                System.exit(-1);
+            }
+        }
 
         // Display the values of inputList.
         for (Integer value : inputList)
