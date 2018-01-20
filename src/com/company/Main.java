416bed4f85bda5742e4f7cd030a9ae4e95ab80bb
kosmidu <kosmidu.maria@gmail.com>
2018-01-20 22:03:52 +0200

    Input data via command line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        System.out.println("Please, enter only non negative integers, separated by spaces.");
+
+        ArrayList<Integer> inputList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        String[] tokens = scanner.nextLine().split("\\s");
+
+        // Insert the integers to inputList.
+        for (String token : tokens)
+            inputList.add(Integer.parseInt(token));
+
+        // Display the values of inputList.
+        for (Integer value : inputList)
+            System.out.print(value + " ");
     }
 }
