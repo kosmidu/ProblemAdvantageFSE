d9f238daad54d25f7f83207b5a12a365a28eba3a
kosmidu <kosmidu.maria@gmail.com>
2018-01-21 12:55:44 +0200

    Add the basic method, findMaxNumber() and myComparator Class. Sort the input list by implementing my own Comparator<>.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,32 @@ import java.util.*;
 
 public class Main
 {
+    private static void findMaxNumber(ArrayList<Integer> integerList)
+    {
+        // Sort integerList with my implementation of Comparator<Integer>.
+        integerList.sort(new myComparator());
+
+        // Display the values of sorted integerList.
+        System.out.print("---> ");
+        for (Integer value : integerList)
+            System.out.print(value + " ");
+
+        // Convert to string
+        StringBuilder sb = new StringBuilder();
+        ListIterator<Integer> i = integerList.listIterator();
+        while(i.hasNext())
+        {
+            sb.append(Integer.toString(i.next()));
+        }
+
+        String string = sb.toString();
+        System.out.println("\n" + string);
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Please, insert only non negative integers, separated by spaces.\nThen press enter.");
+        System.out.println("Please, insert only non negative integers, separated by spaces." +
+                "\nThen press enter.");
 
         ArrayList<Integer> inputList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
@@ -26,5 +49,8 @@ public class Main
         // Display the values of inputList.
         for (Integer value : inputList)
             System.out.print(value + " ");
+
+        // Call the method 'findMaxNumber()'.
+        findMaxNumber(inputList);
     }
 }
---
+++ src/com/company/myComparator.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Comparator;
+
+public class myComparator implements Comparator<Integer>
+{
+    public int compare(Integer a, Integer b)
+    {
+        // Concatenate two integers
+        String ab = Integer.toString(a) + Integer.toString(b);
+        String ba = Integer.toString(b) + Integer.toString(a);
+
+        return Integer.compare(Integer.parseInt(ba),Integer.parseInt(ab));
+    }
+}
